64f0c3e946dcf5d470e1ccb2a4cfb5a99a0c593b
aristotle0x01 <aristotle0x01@protonmail.com>
2022-05-26 16:40:07 +0800

    III 15: vm skeleton

--- vm/main.c
+++ vm/main.c
@@ -1,28 +1,65 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
-int main(int argc, const char* argv[]) {
-  Chunk chunk;
-  initChunk(&chunk);
-
-  int constant = addConstant(&chunk, 5);
-  writeChunk(&chunk, OP_CONSTANT, 123);
-  writeChunk(&chunk, constant, 123);
-
-  writeChunk(&chunk, OP_RETURN, 123);
-
-  disassembleChunk(&chunk, "test chunk");
-  freeChunk(&chunk);
-
+//1 * 2 + 3
+//1 + 2 * 3
+//3 - 2 - 1
+//1 + 2 * 3 - 4 / -5
+static void another(){
+  initVM();
   Chunk chunk2;
   initChunk(&chunk2);
-  writeChunk(&chunk, OP_RETURN, 1);
   for (int i=0; i < 300; i ++) {
       writeConstant(&chunk2, 100+i, 1000+i);
   }
-  writeChunk(&chunk, OP_RETURN, 100);
+  writeConstant(&chunk2, 1, 2000);
+  writeConstant(&chunk2, 2, 2000);
+  writeChunk(&chunk2, OP_MULTIPLY, 2000);
+  writeConstant(&chunk2, 3, 2000);
+  writeChunk(&chunk2, OP_ADD, 2000);
+
+  writeConstant(&chunk2, 1, 2000);
+  writeConstant(&chunk2, 2, 2000);
+  writeConstant(&chunk2, 3, 2000);
+  writeChunk(&chunk2, OP_MULTIPLY, 2000);
+  writeChunk(&chunk2, OP_ADD, 2000);
+  writeConstant(&chunk2, 4, 2000);
+  writeConstant(&chunk2, 5, 2000);
+  writeChunk(&chunk2, OP_NEGATE, 2000);
+  writeChunk(&chunk2, OP_DIVIDE, 2000);
+  writeChunk(&chunk2, OP_SUBTRACT, 2000);
+
+  writeChunk(&chunk2, OP_RETURN, 2001);
   disassembleChunk(&chunk2, "test long constant");
+  interpret(&chunk2);
+  freeVM();
   freeChunk(&chunk2);
+}
+
+int main(int argc, const char* argv[]) {
+//  initVM();
+//  Chunk chunk;
+//  initChunk(&chunk);
+//  int constant = addConstant(&chunk, 1.2);
+//  writeChunk(&chunk, OP_CONSTANT, 123);
+//  writeChunk(&chunk, constant, 123);
+//  constant = addConstant(&chunk, 3.4);
+//  writeChunk(&chunk, OP_CONSTANT, 123);
+//  writeChunk(&chunk, constant, 123);
+//  writeChunk(&chunk, OP_ADD, 123);
+//  constant = addConstant(&chunk, 5.6);
+//  writeChunk(&chunk, OP_CONSTANT, 123);
+//  writeChunk(&chunk, constant, 123);
+//  writeChunk(&chunk, OP_DIVIDE, 123);
+//  writeChunk(&chunk, OP_NEGATE, 123);
+//  writeChunk(&chunk, OP_RETURN, 123);
+//  disassembleChunk(&chunk, "test chunk");
+//  interpret(&chunk);
+//  freeVM();
+//  freeChunk(&chunk);
+
+  another();
   return 0;
 }
---
+++ vm/vm.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "common.h"
+#include "vm.h"
+#include "debug.h"
+
+VM vm;
+
+static void resetStack() {
+  vm.stackTop = vm.stack;
+}
+
+void initVM() {
+    resetStack();
+}
+
+void freeVM() {
+}
+
+void push(Value value) {
+  int diff = vm.stackTop - vm.stack;
+  if (diff >= STACK_MAX) {
+    printf("**** stack overflow ****\n");
+    exit(1);
+  }
+  *vm.stackTop = value;
+  vm.stackTop++;
+}
+
+Value pop() {
+  vm.stackTop--;
+  int diff = vm.stackTop - vm.stack;
+  if (diff < 0) {
+    printf("**** stack underflow ****\n");
+    exit(1);
+  }
+  return *vm.stackTop;
+}
+
+static InterpretResult run() {
+#define READ_BYTE() (*vm.ip++)
+#define READ_CONSTANT(op) (op == OP_CONSTANT ? \
+    vm.chunk->constants.values[READ_BYTE()] : \
+    vm.chunk->constants.values[READ_BYTE() + (READ_BYTE() << 8) + (READ_BYTE() << 16)])
+#define BINARY_OP(op) \
+    do { \
+      double b = pop(); \
+      double a = pop(); \
+      push(a op b); \
+    } while (false)
+
+  for (;;) {
+#ifdef DEBUG_TRACE_EXECUTION
+    printf("          ");
+    for (Value* slot = vm.stack; slot < vm.stackTop; slot++) {
+      printf("[ ");
+      printValue(*slot);
+      printf(" ]");
+    }
+    printf("\n");
+    disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
+#endif
+    uint8_t instruction;
+    switch (instruction = READ_BYTE()) {
+      case OP_CONSTANT: {
+        Value constant = READ_CONSTANT(OP_CONSTANT);
+        push(constant);
+        break;
+      }
+      case OP_CONSTANT_LONG: {
+        Value constant = READ_CONSTANT(OP_CONSTANT_LONG);
+        printf("OP_CONSTANT_LONG: %lf\n", constant);
+        push(constant);
+        break;
+      }
+      case OP_ADD:      BINARY_OP(+); break;
+      case OP_SUBTRACT: BINARY_OP(-); break;
+      case OP_MULTIPLY: BINARY_OP(*); break;
+      case OP_DIVIDE:   BINARY_OP(/); break;
+      case OP_NEGATE:   push(-pop()); break;
+      case OP_RETURN: {
+        printValue(pop());
+        printf("\n");
+        return INTERPRET_OK;
+      }
+    }
+  }
+
+#undef READ_BYTE
+#undef READ_CONSTANT
+#undef BINARY_OP
+}
+
+InterpretResult interpret(Chunk* chunk) {
+  vm.chunk = chunk;
+  vm.ip = vm.chunk->code;
+  return run();
+}
