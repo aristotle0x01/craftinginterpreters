7eacf550bf334586072816d871230b2109ef1b17
aristotle0x01 <aristotle0x01@protonmail.com>
2022-06-08 12:14:21 +0800

    III 23: jump

--- vm/vm.c
+++ vm/vm.c
@@ -96,6 +96,8 @@ static InterpretResult run() {
     vm.chunk->constants.values[READ_BYTE()] : \
     vm.chunk->constants.values[READ_BYTE() + (READ_BYTE() << 8) + (READ_BYTE() << 16)])
 #define READ_CONSTANT1() (vm.chunk->constants.values[READ_BYTE()])
+#define READ_SHORT() \
+    (vm.ip += 2, (uint16_t)((vm.ip[-2] << 8) | vm.ip[-1]))
 #define READ_STRING() AS_STRING(READ_CONSTANT1())
 #define BINARY_OP(valueType, op) \
     do { \
@@ -210,6 +212,21 @@ static InterpretResult run() {
         printf("\n");
         break;
       }
+      case OP_JUMP: {
+        uint16_t offset = READ_SHORT();
+        vm.ip += offset;
+        break;
+      }
+      case OP_JUMP_IF_FALSE: {
+        uint16_t offset = READ_SHORT();
+        if (isFalsey(peek(0))) vm.ip += offset;
+        break;
+      }
+      case OP_LOOP: {
+        uint16_t offset = READ_SHORT();
+        vm.ip -= offset;
+        break;
+      }
       case OP_RETURN: {
         // The bytecode for an entire statement has a total stack effect of zero
         // printValue(pop());
@@ -220,6 +237,7 @@ static InterpretResult run() {
   }
 
 #undef READ_BYTE
+#undef READ_SHORT
 #undef READ_CONSTANT
 #undef READ_STRING
 #undef BINARY_OP
